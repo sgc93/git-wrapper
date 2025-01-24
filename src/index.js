9e4ae2d42f4e3cba9e2193e8f8c216cbab5e328a
sgc93 <smachewgedefaw@gmail.com>
2025-01-24 12:32:30 -0900

    handle retriving user data from a separage handler

--- src/index.js
+++ src/index.js
@@ -1,36 +1,12 @@
-const axios = require("axios");
+const { getUserProfile } = require("./core/profile");
 
-async function getUserInfo(username, token) {
-  console.log(token);
+async function gitWrapped(username, token) {
   try {
-    const url = `https://api.github.com/users/${username}`;
-
-    const headers = token ? { Authorization: `token ${token}` } : {};
-
-    const response = await axios.get(url, { headers });
-
-    const { login, name, bio, location, company, avatar_url, created_at } =
-      response.data;
-
-    return {
-      username: login,
-      name,
-      bio,
-      location,
-      company,
-      avatarUrl: avatar_url,
-      accountCreated: created_at,
-    };
+    const profile = await getUserProfile(username, token);
+    return { profile };
   } catch (error) {
-    if (error.response && error.response.status === 404) {
-      throw new Error(`User "${username}" not found.`);
-    } else if (error.response && error.response.status === 403) {
-      throw new Error(
-        "Rate limit exceeded. Use a personal access token for higher limits."
-      );
-    }
-    throw new Error("An error occurred while fetching user information.");
+    throw new Error(error);
   }
 }
 
-module.exports = { getUserInfo };
+module.exports = { gitWrapped };
