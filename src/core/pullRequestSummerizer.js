d1194b2451fd517c21bda265191f2041ece1dda8
sgc93 <smachewgedefaw@gmail.com>
2025-01-25 04:04:53 -0900

    update autorization name

--- src/core/pullRequestSummerizer.js
+++ src/core/pullRequestSummerizer.js
@@ -3,7 +3,7 @@ const { default: axios } = require("axios");
 const pullRequestSummerizer = async (username, token) => {
   try {
     const url = `https://api.github.com/search/issues?q=author:${username}+type:pr`;
-    const headers = token ? { headers: `token ${token}` } : {};
+    const headers = token ? { headers: `Bearer ${token}` } : {};
 
     const response = await axios.get(url, { headers });
     const pr = response.data.total_count;
