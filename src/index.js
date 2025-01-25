a953f6e843004b07b9cc6a2b099e99ec7dffabd9
sgc93 <smachewgedefaw@gmail.com>
2025-01-24 19:32:16 -0900

    update way of retrieving user data

--- src/index.js
+++ src/index.js
@@ -2,37 +2,22 @@ const { getAllRepos } = require("./api/repos");
 const { getUserProfile } = require("./api/profile");
 const repoSummerizer = require("./core/repoSummerizer");
 const lngSummerizer = require("./core/lngSummerizer");
+const commitSummerizer = require("./core/commitSummerizer");
 
 async function gitWrapped(username, token) {
   try {
-    const profile = await getUserProfile(username, token);
+    // const profile = await getUserProfile(username, token);
     const repos = await getAllRepos(username, token);
-    const { totalRepos, starsEarned, topStarredRepos, topCommitedRepos } =
-      repoSummerizer(repos);
-    const {
-      totalLngs,
-      topThreeLanguages,
-      lngCoverage,
-      lngRepoCoverage,
-      topLanguage,
-    } = await lngSummerizer(repos, token);
+    // const repoStatus = repoSummerizer(repos);
+    // const langugeStatus = await lngSummerizer(repos, token);
+    const commitStatus = await commitSummerizer(
+      repos.slice(0, 2),
+      username,
+      token
+    );
 
     return {
-      profile,
-      stats: {
-        totalRepos,
-        starsEarned,
-        privateRepos: totalRepos - profile.publicRepos,
-        publicRepos: profile.publicRepos,
-      },
-      topRepos: topStarredRepos,
-      languages: {
-        totalLngs,
-        topThreeLanguages,
-        lngCoverage,
-        lngRepoCoverage,
-        topLanguage,
-      },
+      commitStatus,
     };
   } catch (error) {
     throw new Error(error);
