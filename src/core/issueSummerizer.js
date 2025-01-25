68aefd7e4599ba7d9046f13f60f953cd29314934
sgc93 <smachewgedefaw@gmail.com>
2025-01-25 11:16:08 -0900

    handle errors

--- src/core/issueSummerizer.js
+++ src/core/issueSummerizer.js
@@ -1,16 +1,33 @@
 const { default: axios } = require("axios");
+const { getErrorMessage } = require("../utils/format");
+const unknowError = require("../utils/unknownError");
 
 const issueSummerizer = async (username, token) => {
   try {
     const url = `https://api.github.com/search/issues?q=author:${username}+type:issue`;
-    const headers = token ? { headers: `token ${token}` } : {};
+    const headers = token ? { headers: `Bearer ${token}` } : {};
 
     const response = await axios.get(url, { headers });
     const issues = response.data.total_count;
 
-    return issues;
+    return {
+      success: true,
+      data: { issues }
+    };
   } catch (error) {
-    throw new Error(error);
+    if (error.response) {
+      const err = getErrorMessage(error.response.status);
+      return {
+        success: false,
+        error: {
+          code: err.code,
+          message: err.message,
+          details: error.response.data
+        }
+      };
+    }
+
+    return unknowError(error);
   }
 };
 
