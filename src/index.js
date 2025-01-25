e5773fa1936554c59738170759ee1bbbbe780a3b
sgc93 <smachewgedefaw@gmail.com>
2025-01-25 11:08:04 -0900

    handle exporting working methods

--- src/index.js
+++ src/index.js
@@ -1,27 +1,19 @@
-const { getAllRepos } = require("./api/repos");
 const { getUserProfile } = require("./api/profile");
-const repoSummerizer = require("./core/repoSummerizer");
-const lngSummerizer = require("./core/lngSummerizer");
 const commitSummerizer = require("./core/commitSummerizer");
 
 async function gitWrapped(username, token) {
-  try {
-    // const profile = await getUserProfile(username, token);
-    const repos = await getAllRepos(username, token);
-    // const repoStatus = repoSummerizer(repos);
-    // const langugeStatus = await lngSummerizer(repos, token);
-    const commitStatus = await commitSummerizer(
-      repos.slice(0, 2),
-      username,
-      token
-    );
-
+  const profile = await getUserProfile(username, token);
+  if (profile.success) {
     return {
-      commitStatus,
+      success: true,
+      profile: profile.data,
+      methods: {
+        commitSummerizer
+      }
     };
-  } catch (error) {
-    throw new Error(error);
+  } else {
+    return { ...profile };
   }
 }
 
-module.exports = { gitWrapped };
+module.exports = { gitWrapped, commitSummerizer, getUserProfile };
