c7c26d18ca4f0fd2c86ae85405330fda819ffdae
sgc93 <smachewgedefaw@gmail.com>
2025-01-25 00:29:19 -0900

    handle counting pull requests

---
+++ src/core/pullRequestSummerizer.js
@@ -0,0 +1,17 @@
+const { default: axios } = require("axios");
+
+const pullRequestSummerizer = async (username, token) => {
+  try {
+    const url = `https://api.github.com/search/issues?q=author:${username}+type:pr`;
+    const headers = token ? { headers: `token ${token}` } : {};
+
+    const response = await axios.get(url, { headers });
+    const pr = response.data.total_count;
+
+    return pr;
+  } catch (error) {
+    throw new Error("Error while tying to search for pull requests");
+  }
+};
+
+module.exports = pullRequestSummerizer;
