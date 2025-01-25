97799a3b9fe960c1481d49b3714b35952238d6e6
sgc93 <smachewgedefaw@gmail.com>
2025-01-24 15:25:07 -0900

    handle retriving user data and repos stats

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
-const { getUserProfile } = require("./core/profile");
+const getAllRepos = require("./api/repos");
+const { getUserProfile } = require("./api/profile");
+const repoSummerizer = require("./core/repoSummerizer");
 
 async function gitWrapped(username, token) {
   try {
     const profile = await getUserProfile(username, token);
-    return { profile };
+    const repos = await getAllRepos(username, token);
+    const { totalRepos, topStarredRepos, topCommitedRepos } =
+      repoSummerizer(repos);
+
+    return {
+      profile,
+      stats: {
+        totalRepos,
+        privateRepos: totalRepos - profile.publicRepos,
+        publicRepos: profile.publicRepos,
+      },
+    };
   } catch (error) {
     throw new Error(error);
   }
