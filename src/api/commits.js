8a881761183d617cd6103cc80165291abdaacb12
sgc93 <smachewgedefaw@gmail.com>
2025-01-25 10:47:40 -0900

    update way of handling list of commits

--- src/api/commits.js
+++ src/api/commits.js
@@ -1,30 +1,35 @@
 const axios = require("axios");
-
-const getCommitsForDateRange = async (username, token, startDate, endDate) => {
-  const url = `https://api.github.com/search/commits?q=author:${username}+author-date:${startDate}..${endDate}`;
-  const headers = {
-    Authorization: `Bearer ${token}`,
-    Accept: "application/vnd.github.cloak-preview+json",
-  };
-
-  const response = await axios.get(url, { headers });
-  return response.data.total_count;
-};
+const { throwErrorMessage } = require("../utils/format");
+const GitWrapperError = require("../model/GitWrapperError");
 
 const getCommitsBetween = async (username, token, year) => {
-  let totalCommits = 0;
   const startDate = `${year}-01-01`;
   const endDate = `${year}-12-31`;
 
-  const commits = await getCommitsForDateRange(
-    username,
-    token,
-    startDate,
-    endDate
-  );
-  totalCommits = commits;
+  try {
+    const url = `https://api.github.com/search/commits?q=author:${username}+author-date:${startDate}..${endDate}`;
+    const headers = token
+      ? {
+          Authorization: `Bearer ${token}`,
+          Accept: "application/vnd.github.cloak-preview+json"
+        }
+      : {};
 
-  return totalCommits;
+    const response = await axios.get(url, { headers });
+    const totalCommits = response.data.total_count;
+
+    return totalCommits;
+  } catch (error) {
+    if (error.response) {
+      throwErrorMessage(error.response.status, error);
+    } else {
+      throw new GitWrapperError(
+        "NETWORK_ERROR",
+        "A network error occurred. Please check your internet connection.",
+        error.message
+      );
+    }
+  }
 };
 
 module.exports = getCommitsBetween;
